dfb129e35d2add1ed1d21435ddb166f4f86ca8b7
erickoficial69 <erickoficial69@gmail.com>
2021-07-03 02:46:13 -0400

    lista para prueba de rendimiento

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,6 +98,62 @@ const IndexPage = ({resp,resp_posts}:Props) => {
                 ):null
             }
         </div>
+        <div className="pagination_container">
+          {
+            resp.total_pages?
+              currentPage.page > 1?(
+                  <button className="icon-button" onClick={()=>next(currentPage.page-1)} style={{padding:"3px 6px"}} >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title>Anterior</title><path d="M240,424V328c116.4,0,159.39,33.76,208,96,0-119.23-39.57-240-208-240V88L64,256Z" style={{width:'32px',height:'32px',fill:'none',stroke:'#000',strokeLinejoin:'round',strokeWidth:'32px'}}/></svg>
+                  </button>
+              ):(
+                <button className="icon-button" style={{padding:"3px 6px"}} >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title>Anterior</title><path d="M240,424V328c116.4,0,159.39,33.76,208,96,0-119.23-39.57-240-208-240V88L64,256Z" style={{width:'32px',height:'32px',fill:'none',stroke:'#000',strokeLinejoin:'round',strokeWidth:'32px'}}/></svg>
+                  </button>
+              )
+            :null
+          }
+          {
+            resp.total_pages?
+              currentPage.page >= 1 && currentPage.page < parseInt(resp.total_pages) ?(
+                  <button className="icon-button" onClick={()=>next(currentPage.page+1)} style={{padding:"3px 6px"}} >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title>Siguiente</title><path d="M448,256,272,88v96C103.57,184,64,304.77,64,424c48.61-62.24,91.6-96,208-96v96Z" style={{width:'32px',height:'32px',fill:'none',stroke:'#000',strokeLinejoin:'round',strokeWidth:'32px'}}/></svg>
+                  </button>
+              ):currentPage.page >= 1 && currentPage.page == parseInt(resp.total_pages)?(
+                <button className="icon-button" style={{padding:"3px 6px"}} >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title>Siguiente</title><path d="M448,256,272,88v96C103.57,184,64,304.77,64,424c48.61-62.24,91.6-96,208-96v96Z" style={{width:'32px',height:'32px',fill:'none',stroke:'#000',strokeLinejoin:'round',strokeWidth:'32px'}}/></svg>
+                  </button>
+              ):null
+            :null
+          }
+      </div>
+      <div className="pagination_container">
+          {
+            resp.total_pages?
+              statePosts.page > 1?(
+                  <button className="icon-button" onClick={()=>next_posts(statePosts.page-1)} style={{padding:"3px 6px"}} >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title>Anterior</title><path d="M240,424V328c116.4,0,159.39,33.76,208,96,0-119.23-39.57-240-208-240V88L64,256Z" style={{width:'32px',height:'32px',fill:'none',stroke:'#000',strokeLinejoin:'round',strokeWidth:'32px'}}/></svg>
+                  </button>
+              ):(
+                <button className="icon-button" style={{padding:"3px 6px"}} >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title>Anterior</title><path d="M240,424V328c116.4,0,159.39,33.76,208,96,0-119.23-39.57-240-208-240V88L64,256Z" style={{width:'32px',height:'32px',fill:'none',stroke:'#000',strokeLinejoin:'round',strokeWidth:'32px'}}/></svg>
+                  </button>
+              )
+            :null
+          }
+          {
+            resp.total_pages?
+              statePosts.page >= 1 && statePosts.page < parseInt(resp.total_pages) ?(
+                  <button className="icon-button" onClick={()=>next_posts(statePosts.page+1)} style={{padding:"3px 6px"}} >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title>Siguiente</title><path d="M448,256,272,88v96C103.57,184,64,304.77,64,424c48.61-62.24,91.6-96,208-96v96Z" style={{width:'32px',height:'32px',fill:'none',stroke:'#000',strokeLinejoin:'round',strokeWidth:'32px'}}/></svg>
+                  </button>
+              ):statePosts.page >= 1 && statePosts.page == parseInt(resp.total_pages)?(
+                <button className="icon-button" style={{padding:"3px 6px"}} >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title>Siguiente</title><path d="M448,256,272,88v96C103.57,184,64,304.77,64,424c48.61-62.24,91.6-96,208-96v96Z" style={{width:'32px',height:'32px',fill:'none',stroke:'#000',strokeLinejoin:'round',strokeWidth:'32px'}}/></svg>
+                  </button>
+              ):null
+            :null
+          }
+      </div>
     </section>
     <aside>
         {
